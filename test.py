253a087c9106801621a36ee0a8c6cb8166f76cce
AlexNgan <6353647+AlexNgan@users.noreply.github.com>
2022-12-29 18:02:31 -0500

    AutoScan now able to set the target port. Need to figure out how to send commands to the device though.

--- test.py
+++ test.py
@@ -13,24 +13,39 @@ import serial
 import serial.tools.list_ports as ports
 from serial.tools.list_ports import comports 
 
-com_ports = [str(i) for i in ports.comports()]# create a list of ['COM1','COM2'] as str
+
+ser = serial.Serial(
+            baudrate=115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            xonxoff=False,
+            rtscts=True, 
+            dsrdtr=True,
+            timeout=0.5,
+            write_timeout=0.5
+        )
+com_ports = [i.device for i in ports.comports()]
+#com_ports = [str(i) for i in ports.comports()]# create a list of ['COM1','COM2'] as str
 PIDs = [i.pid for i in ports.comports()] #List of product IDs (integers unless 'None')
 VIDs = [i.vid for i in ports.comports()] #List of vendor IDs (integers unless 'None')
 
-selected_port = 'COM6'  #Literal to hold name of port that matches user input.
-ser = serial.Serial(
-    port=selected_port,
-    baudrate=115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    xonxoff=True,
-    timeout=0.5,
-    write_timeout=0.5
-)
-
 def NewCase():
     subprocess.run("cls", shell=True, check=True) #Clears the shell.
+    
+#Function to autodetect and connect to first COM port that matches the ProSim 8
+def AutoFind():  
+    for i in range(len(com_ports)): 
+        if VIDs[i] is not None:
+            if (VIDs[i] == 1027) & (PIDs[i] == 24577):
+                 #Sets the 'target' port as the one associated with these matching IDs
+                ser.port = com_ports[i]
+                print("Fluke Pro Sim 8 found at:",ser.port, "\n")
+                return ser.port
+        elif(i == [len(VIDs)-1 or len(PIDs)-1]):   #When at end of list of COM ports.
+            print("No Device Found.\n")
+        else:
+            continue
 
 #Debug function in case the port auto-select fails.
 def ScanPorts(): 
@@ -40,31 +55,47 @@ def ScanPorts():
     for i, (name, vid, pid) in enumerate(sorted(comports()), 1):             
        sys.stderr.write('--- {:2}: {:20} {}\n'.format(name, vid, pid))
 
-    selected_port = input('--- Enter port index or full name: ')
+    global ser
+    ser.port = input('--- Enter port index or full name: ')
     try:
-        index = int(selected_port) - 1 #THIS WILL NOT WORK, FIX TOMORROW...SELECTED PORT IS A LITERAL AND I NEED TO MAKE THE USER INPUT PART MAP COM PORT NAMES TO OPTIONS 1, 2, 3, ETC.
+        index = int(ser.port) - 1 #THIS WILL NOT WORK, FIX TOMORROW...SELECTED PORT IS A LITERAL AND I NEED TO MAKE THE USER INPUT PART MAP COM PORT NAMES TO OPTIONS 1, 2, 3, ETC.
         if not 0 <= index < len(com_ports):
             sys.stderr.write('--- Invalid index!\n')        
     except ValueError:
         pass        
     else:
-        selected_port = com_ports[index]
+        ser.port = com_ports[index]
 
-#Function to autodetect and connect to first COM port that matches the ProSim 8
-def AutoFind():  
-    for i in range(len(com_ports)): 
-        if VIDs[i] is not None:
-            if (VIDs[i] == 1027) & (PIDs[i] == 24577):
-                selected_port = ports.comports()[i]
-                print("Fluke Pro Sim 8 found at:",selected_port, "\n")
-                break
-        elif(i == [len(VIDs)-1 or len(PIDs)-1]):   #When at end of list of COM ports.
-            print("No Device Found.\n")
-        else:
-            continue
+#Takes the target serial port as an argument.
+def OpenSesame():
+    global ser 
+    try:
+        ser.close()
+        ser.open()
+    except:
+        pass
+    return ser
+# try:
+#     ser = serial.Serial(
+#         port=selected_port,
+#         baudrate=115200,
+#         parity=serial.PARITY_NONE,
+#         stopbits=serial.STOPBITS_ONE,
+#         bytesize=serial.EIGHTBITS,
+#         xonxoff=False,
+#         timeout=0.5,
+#         write_timeout=0.5
+#     )
+    
+# except:
+#     pass
 
 NewCase()
 AutoFind()
-print("Open:", ser.isOpen())
-ser.write('IDENT'.encode())
-#print("reply:", (ser.read()).decode())
+print("Target:", AutoFind())
+print("Serial ports:", com_ports)
+#OpenSesame()
+ser.open()
+print(ser)
+ser.write(b'IDENT\r')
+print("reply:", (ser.read()).decode())
