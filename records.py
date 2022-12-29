abedbf342b5b4a4d22d97982fa02ff06cf984448
AlexNgan <6353647+AlexNgan@users.noreply.github.com>
2022-12-29 13:49:26 -0500

    Separate file for functions related to writing the case into a .txt file.

---
+++ records.py
@@ -0,0 +1,11 @@
+import inspect
+import subprocess
+import sys
+import serial
+import serial.tools.list_ports as ports
+from serial.tools.list_ports import comports 
+
+#Creates output txt file, need to write in code to capture user input for participant number
+def GenerateReport(): 
+    with open('output' + VARIABLE + '.txt', 'w') as f:
+        out = subprocess.run('ping 127.0.0.1', shell=True, stdout=f, text=True)
