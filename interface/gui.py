a9670e05b3e39e43a6da12e52cfee27b710f968e
AlexNgan <6353647+AlexNgan@users.noreply.github.com>
2023-01-12 09:58:13 -0500

    Working on interface for easy control. Need to make tasks into methods.

--- interface/gui.py
+++ interface/gui.py
@@ -3,58 +3,81 @@ I'm aware this is jank, that I have to write all the gui code here instead of ca
 """
 
 import tkinter as tk
-from tkinter.ttk import *
-from tkinter import *
+from tkinter import ttk
+import numpy as np
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        #self.pack()
+class App(object):
+    def __init__(self):
+        super().__init__()
+
+        self.geometry("200x200")
+
+        style =ttk.Style(self)
+
+        '''
+        Place below into class when testing is done. Instantiate App in main.py
+        '''
+        window = tk.Tk()
+        window.geometry("300x300")
+        window.title("Fluke ProSim 8 Control Interface")
+        style = ttk.Style()
+
+        #Iterates through rows so that automatic element placement doesn't overlap anything.
+        openRow = 0
+        openColumn = 0
+
+        def getRow():
+            global openRow
+            openRow = openRow+1
+            return openRow
+
+        def getColumn():
+            global openColumn
+            openColumn = openColumn+1
+            return openColumn
+
+        #Function to arrange elements in grid w default values.
+        def place(element, row=0, column=0, pad=5):
+            element.grid(row=row, column=column, pady=pad)
+
+        def manual_entry(parameter):
+            value = tk.Entry(text="Enter " + parameter + " value:\n")
+            value.pack()
+            return value
+
+        def listen(self):
+            self.get()
+
+        '''
+        Label styling.
+        '''
+        style.configure("TLabel",
+            font="comic sans")
         
-    #Application.manual_entry("SpO2")
+        #Map function for dynamic changes based on state.
+        style.map("TButton",
+            foreground=[('pressed', 'red'), ('active', 'blue')],
+            background=[('pressed', '!disabled', 'black'), ('active', 'white')]
+        )
 
-#call this in main for every parameter, cmd should call specific serial functions?
-def button(self,buttontext, color, cmd):
-    self.b = tk.Button(self)
-    self.b["text"] = buttontext
-    self.b["fg"] = color
-    self.b["command"] = cmd
-    self.b.pack()
-    """ b = app.Button(
-            text=buttontext,
-            command=cmd,
-            width=5,
-            height=1,
-            fg=color, #get hex values for colors
-    )
-    b.pack() """
-    
-def manual_entry(parameter):
-    value = tk.Entry(text="Enter " + parameter + " value:\n")
-    value.pack()
-    return value
+        #Configure function for basic formatting.    
+        style.configure("TButton", 
+            padding=6, 
+            relief="flat",
+            background="#ccc",
+            font=[("arial", 7,"bold")]
+        )
 
-def label(self, txt):
-    heading = Label(
-        self, 
-        text=txt, 
-        height=5,
-        width=100
-    )
-    heading.pack()
+        style.configure("TLabel",
+            borderwidth=5,
+            relief="flat",
+            anchor="w" #options are compass based
+        )
 
-def listen(self):
-    self.get()
+        #Refreshes window with any changes.
+        def refresh():
+            tk.Tk().update()
 
-#grab this and slap into main when done?
-root = tk.Tk()
-root.title("ProSim 8 Vitals Sign Simulator")
-heading = label(root,"test")
-app = Application(master=root)
-ox = manual_entry("SpO2")
-spo2 = button(root,"SpO2", "green", NONE)
-quit = button(root, "QUIT", "red",root.destroy)
-
-app.mainloop()
-
+        #Function to arrange elements in grid w default values.
+        def place(element, row=0, column=0, pad=5):
+            element.grid(row=row, column=column, pady=pad)
--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 #The main code will live here, functions will be mapped to keys
 import subprocess
 import sys
+import tkinter as tk
+from tkinter import ttk
+
+import numpy as np
 import serial
 import serial.tools.list_ports as ports
 from serial.tools.list_ports import comports
+
 from prosim8.prosim import PROSIM
-import tkinter as tk
-from tkinter.ttk import *
-from tkinter import *
 
 '''
 **********  These are tools/functions to debug, if necessary.  **********
@@ -27,7 +29,8 @@ def NewCase():
 
 
 '''
-**********  "Actual" code lives below this.  **********
+######################################### Code for Sim #########################################
+-----------------------------------------------------------------------------------------------
 '''
 com_ports = [i.device for i in ports.comports()]
 PIDs = [i.pid for i in ports.comports()] #List of product IDs (integers unless 'None')
@@ -40,27 +43,139 @@ def AutoFind():
     for i in range(len(com_ports)): 
         if VIDs[i] is not None:
             if (VIDs[i] == 1027) & (PIDs[i] == 24577):
-                #PROSIM.port = com_ports[i]
                 print("Fluke Pro Sim 8 found at:", com_ports[i], "\n")
-                # change com port when connected
                 return com_ports[i]
         else:
             print("No Device Found. Please confirm that the simulator is plugged into this computer.\n")
             continue
 
-def MakeUI(self):
-    window = Application()
-    window.label()
-    window.quit = tk.Button(
-        text="QUIT", fg="red",
-        command=self.master.destroy
-        )
-    gui.button("Task(s) 1-3", "green", )
-    #Application.manual_entry("SpO2")
+def initial():
+    ProSim.spo2.set_spo2_sat(85)
+
+def changeFiO2():
+    entry = ttk.Entry(text="New FiO2:")
+    entry.place(entry, getRow(), getColumn())
+    ProSim.spo2.FiO2_adjust(input)
+
+    #App.manual_entry("SpO2")
+
+'''
+######################################### Code for GUI #########################################
+-----------------------------------------------------------------------------------------------
+'''  
+class App(object):
+    def __init__(self):
+        super().__init__()
+
+        style =ttk.Style(self)
+
+        '''
+        Label styling.
+        '''
+        style.configure("TLabel",
+            font="comic sans")
+
+        #Refreshes window with any changes.
+        def refresh():
+            tk.Tk().update()
+
+        #Function to arrange elements in grid w default values.
+        def place(element, row=0, column=0, pad=5):
+            element.grid(row=row, column=column, pady=pad)
+
+
+window = tk.Tk()
+window.geometry("500x300")
+window.title("Fluke ProSim 8 Control Interface")
+style = ttk.Style()
+
+lbl = ttk.Label(text="This will control stuff. Current simulated values:", anchor="w")
+
+#Iterates through rows so that automatic element placement doesn't overlap anything.
+openRow = 0
+openColumn = 0
+
+#Array of parameter names and associated values.
+params = np.array([
+    ["HR:", 72],
+    ["RR:", 15],
+    ["SpO2:", 85]
+])     
+
+
+def updateMsg():
+    global sysmsg
+    #sysmsg = str(message)
+    sysmsg = tk.Message(window, text="Button Pressed.")
+    sysmsg.grid(row=getRow(), column=3)
+
+def getRow():
+    global openRow
+    openRow = openRow+1
+    return openRow
+
+def getColumn():
+    global openColumn
+    openColumn = openColumn+1
+    return openColumn
+
+lbl = ttk.Label(text="This will control stuff. Current simulated values:", anchor="w")
+btn = ttk.Button(text="SEND", command=updateMsg())
+#task = ttk.Checkbutton(text="Task 1")
+
+#Map function for dynamic changes based on state.
+style.map("TButton",
+    foreground=[('pressed', 'red'), ('active', 'blue')],
+    background=[('pressed', '!disabled', 'black'), ('active', 'white')]
+    )
+
+#Configure function for basic formatting.    
+style.configure("TButton", 
+    padding=10, 
+    relief="flat",
+    background="#ccc",
+    font=[("arial", 7,"bold")]
+    )
+
+style.configure("TLabel",
+    borderwidth=5,
+    relief="flat",
+    anchor="w" #options are compass based
+    )
+
+#Function to arrange elements in grid w default values.
+def place(element, row=0, column=0, pad=5):
+    element.grid(row=row, column=column, pady=pad)
+
+lbl.grid(
+    row=0,
+    column=0,
+    columnspan=3,
+    padx=50,
+    pady=10
+    )
+
+#Makes the parameters and their values line up.
+for row in range(params.shape[0]):
+    for col in range(params.shape[1]):
+        label = ttk.Label(text=params[row,col], justify="left")
+        label.grid(sticky="E", row=openRow+1,column=0) #THIS GETS ME THE ALIGNMENT I WANT, BUILD THIS INTO PLACE FUNCTION
+        #place(label, openRow+1, 0)
+    place(label, getRow(), col)
 
 NewCase()
-AutoFind()
 ProSim = PROSIM(AutoFind(), debug=True)
-ProSim.spo2.set_spo2_sat(69) 
-ProSim.spo2.sat()
-ProSim.ecg.ecgrun() # turn on ECG 
+#place(btn, getRow())
+#task.grid(row=getRow(), column=2)
+#window.messagebox.askyesnocancel(title=None, message=None, **options)
+
+lbl = ttk.Label(text="This will control stuff. Current simulated values:", anchor="w")
+btn = ttk.Button(text="SEND", command=updateMsg())
+#task = ttk.Checkbutton(text="Task 1")
+sysmsg = tk.Message(window, text="")
+
+btn.grid(row=getRow(), column=0, columnspan=2)
+btn_row = (btn.grid_info())["row"]
+
+sysmsg.grid(row=getRow(), column=3)
+window.mainloop()
